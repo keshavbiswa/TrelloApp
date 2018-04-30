93bfefd0742e14146fffdc198596bc0d0c127ef6
Keshav <keshavbiswa21@gmail.com>
2018-04-30 18:17:08 +0530

    Completed boards crud functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @boards = current_user.boards
   end
 
   def contact
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,4 @@
 class Board < ApplicationRecord
   has_and_belongs_to_many :users
+  enum scope: {Public: 0, Private: 1}
 end
