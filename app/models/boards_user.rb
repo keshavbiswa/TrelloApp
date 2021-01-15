7ac126c72fd2407101357fe2308fcad1970da8c6
Keshav Biswa <keshavbiswa21@gmail.com>
2021-01-15 11:01:27 +0530

    removed :

--- app/models/boards_user.rb
+++ app/models/boards_user.rb
@@ -2,6 +2,6 @@ class BoardsUser < ApplicationRecord
   belongs_to :user
   belongs_to :board
 
-  validate_uniqueness_of: :users
-  validate_uniqueness_of: :boards
+  validate_uniqueness_of :users
+  validate_uniqueness_of :boards
 end
